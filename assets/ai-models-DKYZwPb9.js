dbdcdcaff29fc9d2c23825f6a2ee882aff3da028
TienxDun <TienxDun@users.noreply.github.com>
2026-01-17 09:45:41 +0000

    deploy: 3e1ebef74f9280eaa53e4edcef0ca4803e627208

---
+++ assets/ai-models-DKYZwPb9.js
@@ -0,0 +1,1 @@
+const e=[{id:"gemini-2.5-flash-lite",name:"Gemini 2.5 Flash Lite",category:"Text-out models",rpm:"2",tpm:"1.6K",rpd:"12"},{id:"gemini-2.5-flash",name:"Gemini 2.5 Flash",category:"Text-out models",rpm:"1",tpm:"508",rpd:"3"},{id:"gemini-3-flash",name:"Gemini 3 Flash (Preview)",category:"Text-out models",rpm:"1",tpm:"114",rpd:"1"}];export{e as A};
